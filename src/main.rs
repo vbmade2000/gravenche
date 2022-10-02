2df78c7e44e3a2e31b47d188aa646a15c5d1abcf
Malhar Vora <mlvora.2010@gmail.com>
2022-10-02 17:38:45 +0530

    Read CSV file
    This commit adds a functionality to read CSV file using csv crate. It uses
    use of BufReader to avoid multiple read calls to file system. Also, it uses
    a single variable to avoid memory allocation every time the loop goes on.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, io::Error, path::PathBuf, process};
+use std::{
+    env,
+    fs::File,
+    io::{BufReader, Error},
+    path::PathBuf,
+    process,
+};
 
 /// Processes command line arguments. Exits the process with code 1 if argument count is less than 2.
 fn get_command_line_args() -> Vec<String> {
@@ -16,6 +22,24 @@ fn get_csv_path(filename: &str) -> Result<PathBuf, Error> {
     std::path::Path::new(filename).canonicalize()
 }
 
+/// Read CSV file
+fn read_csv(csv_filepath: &PathBuf) {
+    let file = File::open(csv_filepath).unwrap();
+
+    // Use of BufReader prevents
+    let buf_reader = BufReader::new(file);
+
+    // We re-use this store record.
+    let mut record = csv::StringRecord::new();
+
+    let mut csv_reader = csv::Reader::from_reader(buf_reader);
+
+    // Using an existing variable to store a record prevents memory allocation every time.
+    while csv_reader.read_record(&mut record).unwrap() {
+        println!("-> {:?}", &record);
+    }
+}
+
 fn main() {
     // Process command line args
     let args = get_command_line_args();
@@ -23,5 +47,7 @@ fn main() {
     // Get absolute path of CSV filename.
     let csv_filepath = get_csv_path(&args[1]).unwrap();
 
-    println!("{:?}", csv_filepath);
+    // println!("{:?}", csv_filepath);
+
+    read_csv(&csv_filepath);
 }
