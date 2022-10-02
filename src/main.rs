986006a536466d41baec2c8e4833157044342ad6
Malhar Vora <mlvora.2010@gmail.com>
2022-10-02 17:19:19 +0530

    Handle command line arguments
    This commit adds functionality to accept CSV file path from command line
    arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env, io::Error, path::PathBuf, process};
+
+/// Processes command line arguments. Exits the process with code 1 if argument count is less than 2.
+fn get_command_line_args() -> Vec<String> {
+    // Process command line args
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("ERROR: Please provide csv filename");
+        process::exit(1);
+    }
+    args
+}
+
+/// Generates absolute path for supplied csv filename. Also, checks if filename exists.
+fn get_csv_path(filename: &str) -> Result<PathBuf, Error> {
+    std::path::Path::new(filename).canonicalize()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Process command line args
+    let args = get_command_line_args();
+
+    // Get absolute path of CSV filename.
+    let csv_filepath = get_csv_path(&args[1]).unwrap();
+
+    println!("{:?}", csv_filepath);
 }
